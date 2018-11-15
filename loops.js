31670e53ff57f1a1c770b02036c5d40c287681b1
Tali Scheer <tali.scheer@gmail.com>
2018-11-15 14:31:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ var str = `I am ${i} strange loops.`;
 function forLoop(arr) {
    for (let i = 0; i < 25; i++) {
       if(i === 1) { 
-  console.log("I am 1 strange loop.");
+  console.log(str.slice(0,19) + strangeArray.slice(20));
       } else {
         console.log(str);
   }  
