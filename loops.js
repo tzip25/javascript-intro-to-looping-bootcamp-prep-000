9ccd21cb0ea29257553aa630c16bd173f66f5333
Tali Scheer <tali.scheer@gmail.com>
2018-11-15 14:30:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -5,7 +5,7 @@ function forLoop(arr) {
       if(i === 1) { 
   console.log("I am 1 strange loop.");
       } else {
-        console.log("I am " + i + " strange loops.");
+        console.log(str);
   }  
   }
 return arr;
