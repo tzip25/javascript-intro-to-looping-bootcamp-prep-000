bf7f106a5d3ace787f2e901051ab28a476b9cb1c
Tali Scheer <tali.scheer@gmail.com>
2018-11-15 14:22:50 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(i) {
+   for (let i = 0; i < 25; i++) {
+      if(i === 1) { 
+  console.log("I am " + i + " strange loop.");
+      } else {
+        console.log("I am " + i + " strange loops.");
+  }  
+  }
+return i;
+}
+
+
+function whileLoop(a) {
+ while (a > 0) {
+  console.log(--a);
+  }  
+  return `done`;
+}
+
+
+ function incrementVariable(b) {
+       b.splice(-1);
+}
+
+function doWhileLoop(b) {
+do {
+  incrementVariable(b);
+} while (b.length > 0 );
+return b;
+}
